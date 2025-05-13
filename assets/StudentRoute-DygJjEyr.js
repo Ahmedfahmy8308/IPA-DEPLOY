5f3e5a0c3b76c255114ce24ae03e51f76415821a
mohamed rashad <eldok62882@gmail.com>
2025-05-13 23:26:04 +0300

    oj[

---
+++ assets/StudentRoute-DygJjEyr.js
@@ -0,0 +1,1 @@
+import{n as s,j as o,N as n}from"./index-BrkXOzuL.js";const u=({children:e})=>{const{session:t}=s();return!t||!t||t.role!=="Student"?o.jsx(n,{to:"/not-found",replace:!0}):e};export{u as default};
