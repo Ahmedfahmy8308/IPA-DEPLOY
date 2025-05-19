317cbda069a199793479ecd059b7ec0a8b1097c6
Ahmedfahmy8308 <fhmy8308@gmail.com>
2025-05-20 00:59:26 +0300

    cx

---
+++ assets/StudentRoute-BkKf875H.js
@@ -0,0 +1,1 @@
+import{n as s,j as o,N as n}from"./index-BFIRCOFm.js";const u=({children:e})=>{const{session:t}=s();return!t||!t||t.role!=="Student"?o.jsx(n,{to:"/not-found",replace:!0}):e};export{u as default};
